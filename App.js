e860c41fc29acb48d2cf20c73c913b885a1ee9ef
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-10-26 22:20:43 -0300

    resolvendo

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import AppNavigator from './src/navigation/Appnavigator';
+import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
   return (
