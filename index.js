056b41972dba569d36ed1ff10e4eb257bb27caeb
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-10-28 10:57:02 -0300

    resolvendo

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+// index.js
 import { AppRegistry } from 'react-native';
 import App from './App';
 
 const appName = require('./app.json').expo.name;
 
-// === DESATIVA TUDO QUE CAUSA O ERRO VERMELHO ===
-global.__turboModulesDisabled = true;
-global.__useTurboModules = false;
-global.__experimentalTurboModules = false;
-global.__bridgeLess = false; // Nova Architecture
-// =============================================
+// === REMOVA ESTAS LINHAS: ===
+// global.__EXPO_USE_LEGACY_BRIDGE = true;
+// global.__turboModulesDisabled = true;
+// global.__useTurboModules = false;
+// global.__bridgeLess = false;
+// =============================
 
+// Registra o app
 AppRegistry.registerComponent(appName, () => App);
