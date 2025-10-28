447c1eb19300e477981afb2a15d8bd996749d2a0
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-10-28 14:19:33 -0300

    resorvendo erro ja que o json não le certo

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-// Desativa TurboModules no modo dev (resolve erro vermelho)
+// Usa require() em vez de import (funciona 100% com JSON)
+const appName = require('./app.json').expo.name;
+
+// Desativa TurboModules no modo dev
 if (__DEV__) {
   global.__turboModulesDisabled = true;
 }
 
-// Registro seguro
+// Registra o app
 AppRegistry.registerComponent(appName, () => App);
