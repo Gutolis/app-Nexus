74ce625dc36b0806ec0ce3dc50384f610d8c838a
Gutolis <padilha.augusto@escola.pr.gov.br>
2025-10-28 13:55:15 -0300

    resolvendo

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-// Desativa TurboModules no modo desenvolvimento (resolve o erro vermelho)
+// Desativa TurboModules no modo dev (resolve erro vermelho)
 if (__DEV__) {
   global.__turboModulesDisabled = true;
 }
 
+// Registro seguro
 AppRegistry.registerComponent(appName, () => App);
